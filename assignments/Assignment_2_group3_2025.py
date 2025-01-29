4406ac766099690bfab691affba864eecde7809b
sdiaz06 <sdiazq@pucp.edu.pe>
2025-01-28 22:59:01 -0500

    Update Assignment_2_group3_2025.py

--- assignments/Assignment_2_group3_2025.py
+++ assignments/Assignment_2_group3_2025.py
@@ -1,3 +1,12 @@
 # This file contains the solution for Assignment 2
 for x in range(1, 101):
     print(x)
+for x in range(1, 101):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    elif x % 3 == 0:
+        print("Fizz")
+    elif x % 5 == 0:
+        print("Buzz")
+    else:
+        print(x)
